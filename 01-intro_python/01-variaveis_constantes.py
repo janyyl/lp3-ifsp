8a13a31ab3bc21eb5d8058c9bbdb34930e58a100
Estudante 1 <estudante1@debian.spo.ifsp.edu.br>
2024-02-28 14:34:07 -0300

    variaveis e constantes

---
+++ 01-intro_python/01-variaveis_constantes.py
@@ -0,0 +1,53 @@
+# Identificadores 
+# Letra, numero e ...
+# Não pode ser palavra reservada: if, nome, true. false
+# case sensitive 
+    nome = "Pedro"
+    nome = "Pedro"
+
+#variaveis 
+# tudo em minusculo
+# separador _
+# skane_case
+
+idade = 20
+pessoa_fisica = "Marcio"
+pessoa_juridica = "Paulo LTDA"
+imposto_renda = 2200.45
+
+# E o tipo?
+# Java
+# String nome = Pedro
+# int idade = 20
+# No python temos inferencia de tipo
+# o tipo será definido com base no valor (literal)
+idade = 20 # int
+nome = "Pedro" # Str
+
+# Constantes
+# Não esiste constantes em Python
+# Convenção: nome em maiusculo
+PI = 3.14
+
+# Comentarios de uma unica linha 
+
+'''
+comentarios de multiplas 
+linhas
+'''
+
+# Docstring (comentario de documentação)
+# Documentar classe, modulos, funções, ...
+
+# Exemplo Java
+static somar(double numero1, double numero2) {
+    return numero1 + numero2
+}
+
+
+def somar (numero1,numero2):
+    '''
+    Função que soma dois 
+    números
+    '''
+    return numero1 + numero2
